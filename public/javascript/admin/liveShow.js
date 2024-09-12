7cc283a996767c820bcd713519d461823f0a9206
usuRiku <susukio.r1030@gmail.com>
2024-09-13 02:44:49 +0900

    autoscroll機能を500に設定

--- public/javascript/admin/liveShow.js
+++ public/javascript/admin/liveShow.js
@@ -6,7 +6,7 @@ const sortable1 = Sortable.create(el, {
     scroll: true,
     scrollSensitivity: 200,
     forceFallback: true,
-    scrollSpeed: 2000,
+    scrollSpeed: 500,
     handle: ".handle"
 });
 
